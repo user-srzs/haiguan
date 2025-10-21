428bdb216476f3ee10543bd990e15785e608e0e7
sY.zhang <srzs0408@126.com>
2025-10-21 19:27:43 +0800

    feat: 二次封装el-button组件

---
+++ src/components/CustomButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <component :is="h(ElButton, { ...$attrs, ...props, ref: changeRef }, $slots)"></component>
+</template>
+
+<script setup lang="ts">
+import { ElButton } from "element-plus";
+
+const props = defineProps({});
+
+const vm = getCurrentInstance();
+
+/**
+ * @param {Record<string, any> | null} instance
+ */
+const changeRef = (instance) => {
+  vm.exposed = vm.exposeProxy = instance || {};
+}
+
+</script>
+
+<style scoped lang="scss">
+
+</style>
